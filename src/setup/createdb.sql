973bbc473aa68dee3bb1a29e9c99700c48984c8b
Kreisverkehr <mm.rueter@gmail.com>
2021-06-21 09:02:28 +0000

    fixed createdb.sql

--- src/setup/createdb.sql
+++ src/setup/createdb.sql
@@ -1,9 +1,7 @@
 CREATE TABLE `EatMan`.`tags` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE INDEX `name_UNIQUE` (`name` ASC) VISIBLE)
-ENGINE = InnoDB
+  PRIMARY KEY (`id`))
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_bin;
 
@@ -14,7 +12,6 @@ CREATE TABLE `EatMan`.`dishes` (
   `recipe_location` VARCHAR(1000) NULL,
   `cooking_time` DECIMAL(5,1) NULL,
   PRIMARY KEY (`id`))
-ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_bin;
 
@@ -22,7 +19,6 @@ CREATE TABLE `EatMan`.`dish_tag` (
   `dish` INT NOT NULL,
   `tag` INT NOT NULL,
   PRIMARY KEY (`dish`, `tag`),
-  INDEX `fk_tag_idx` (`tag` ASC) VISIBLE,
   CONSTRAINT `fk_dish`
     FOREIGN KEY (`dish`)
     REFERENCES `EatMan`.`dishes` (`id`)
@@ -33,6 +29,5 @@ CREATE TABLE `EatMan`.`dish_tag` (
     REFERENCES `EatMan`.`tags` (`id`)
     ON DELETE CASCADE
     ON UPDATE CASCADE)
-ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_bin;
